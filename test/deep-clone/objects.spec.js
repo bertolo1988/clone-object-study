6be448192184c58f6cb4cc177db5eb873ac51f6d
bertolo1988 <tiagobertolo@gmail.com>
2022-10-02 23:29:55 +0100

    Merge objects test.

--- test/deep-clone/objects.spec.js
+++ test/deep-clone/objects.spec.js
@@ -99,5 +99,100 @@ describe.each(funcs)('objects part1', (func) => {
 
       assert.strictEqual(clone.bar, 'foo-bar')
     })
+
+    it('should clone enumerable properties (same as shallow clone)', () => {
+      const input = Object.create(Object.prototype, {
+        property1: {
+          value: 42,
+          enumerable: true
+        }
+      })
+      const clone = func(input)
+      assert.equal(
+        Object.getOwnPropertyDescriptor(clone, 'property1')?.enumerable,
+        true
+      )
+    })
+
+    it('should clone non enumerable properties', () => {
+      const input = Object.create(Object.prototype, {
+        property1: {
+          value: 42,
+          enumerable: false
+        }
+      })
+      const clone = func(input)
+      assert.equal(
+        Object.getOwnPropertyDescriptor(clone, 'property1')?.enumerable,
+        false
+      )
+    })
+
+    it('should clone enumerable and writable properties', () => {
+      const input = Object.create(Object.prototype, {
+        property1: {
+          value: 42,
+          enumerable: true,
+          configurable: false,
+          writable: true
+        }
+      })
+      const clone = func(input)
+      const cloneOwnDescriptor = Object.getOwnPropertyDescriptor(
+        clone,
+        'property1'
+      )
+      assert.ok(cloneOwnDescriptor != null)
+      assert.equal(cloneOwnDescriptor.enumerable, true)
+      assert.equal(cloneOwnDescriptor.configurable, false)
+      assert.equal(cloneOwnDescriptor.writable, true)
+    })
+
+    it('should clone enumerable and configurable properties', () => {
+      const input = Object.create(Object.prototype, {
+        property1: {
+          value: 42,
+          enumerable: true,
+          configurable: true,
+          writable: false
+        }
+      })
+      const clone = func(input)
+      const cloneOwnDescriptor = Object.getOwnPropertyDescriptor(
+        clone,
+        'property1'
+      )
+      assert.ok(cloneOwnDescriptor != null)
+      assert.equal(cloneOwnDescriptor.enumerable, true)
+      assert.equal(cloneOwnDescriptor.configurable, true)
+      assert.equal(cloneOwnDescriptor.writable, false)
+    })
+
+    it('should clone symbol properties', () => {
+      const input = { [Symbol('a')]: 44 }
+      const clone = func(input)
+
+      const cloneOwnPropertySymbols = Object.getOwnPropertySymbols(clone)
+
+      assert.ok(cloneOwnPropertySymbols.length === 1)
+      assert.equal(
+        cloneOwnPropertySymbols[0].toString(),
+        Symbol('a').toString()
+      )
+    })
+
+    it('should clone frozen object', () => {
+      const input = { foo: 'bar' }
+      Object.freeze(input)
+      const clone = func(input)
+      assert.ok(Object.isFrozen(clone))
+    })
+
+    it('should clone sealed object', () => {
+      const input = { foo: 'bar' }
+      Object.seal(input)
+      const clone = func(input)
+      assert.ok(Object.isSealed(clone))
+    })
   })
 })
