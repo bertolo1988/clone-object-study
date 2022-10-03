fdef1cfdbf53316d4f931aab7a73060025293af2
bertolo1988 <tiagobertolo@gmail.com>
2022-10-04 00:45:08 +0100

    Add map and set.

---
+++ test/deep-clone/map.spec.js
@@ -0,0 +1,55 @@
+const assert = require('node:assert')
+const _ = require('lodash')
+const { mapClassStaticMethods } = require('../test-utils')
+const DeepClone = require('../../src/functions/deep-clone')
+
+const funcs = mapClassStaticMethods(DeepClone)
+
+describe.each(funcs)('Map', (func) => {
+  describe(`🟣 ${func.name} `, () => {
+    it('should deeply clone Map', () => {
+      const a = new Map([[1, 5]])
+      const b = func(a)
+      assert.deepEqual(Array.from(a), Array.from(b))
+      a.set('cc', { f: 'bar' })
+      assert.notDeepEqual(Array.from(a), Array.from(b))
+    })
+
+    it('should clone the constructor', () => {
+      const a = new Map([
+        ['foo', 'bar'],
+        ['a', 'b']
+      ])
+      const b = func(a)
+      assert.ok(a.constructor.name === b.constructor.name)
+    })
+
+    it('should clone the prototype', () => {
+      const a = new Map([
+        ['foo', 'bar'],
+        ['a', 'b']
+      ])
+      const b = func(a)
+      assert.deepEqual(a.prototype, b.prototype)
+    })
+
+    it('should clone the size property', () => {
+      const a = new Map([
+        ['foo', 'bar'],
+        ['a', 'b']
+      ])
+      const b = func(a)
+      assert.ok(a.size === b.size)
+    })
+
+    it('should clone own properties if they exist', () => {
+      const a = new Map([
+        ['foo', 'bar'],
+        ['a', 'b']
+      ])
+      a.foo = 'bar'
+      const b = func(a)
+      assert.ok(a.foo === b.foo)
+    })
+  })
+})
---
+++ test/deep-clone/set.spec.js
@@ -0,0 +1,43 @@
+const assert = require('node:assert')
+const _ = require('lodash')
+const { mapClassStaticMethods } = require('../test-utils')
+const DeepClone = require('../../src/functions/deep-clone')
+
+const funcs = mapClassStaticMethods(DeepClone)
+
+describe.each(funcs)('Set', (func) => {
+  describe(`🟣 ${func.name} `, () => {
+    it('should deeply clone Set', () => {
+      const a = new Set([2, 1, 3])
+      const b = func(a)
+      assert.deepEqual(Array.from(a), Array.from(b))
+      a.add(8)
+      assert.notDeepEqual(Array.from(a), Array.from(b))
+    })
+
+    it('should clone the constructor', () => {
+      const a = new Set([2, 1, 3])
+      const b = func(a)
+      assert.ok(a.constructor.name === b.constructor.name)
+    })
+
+    it('should clone the prototype', () => {
+      const a = new Set([2, 1, 3])
+      const b = func(a)
+      assert.deepEqual(a.prototype, b.prototype)
+    })
+
+    it('should clone the size property', () => {
+      const a = new Set([2, 1, 3])
+      const b = func(a)
+      assert.ok(a.size === b.size)
+    })
+
+    it('should clone own properties if they exist', () => {
+      const a = new Set([2, 1, 3])
+      a.foo = 'bar'
+      const b = func(a)
+      assert.ok(a.foo === b.foo)
+    })
+  })
+})
