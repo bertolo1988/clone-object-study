09f02a2723ad017e24be79fdfb0dc547db27bc8a
bertolo1988 <tiagobertolo@gmail.com>
2022-10-05 00:58:04 +0100

    Add more tests.

--- test/deep-clone/functions.spec.js
+++ test/deep-clone/functions.spec.js
@@ -5,7 +5,7 @@ const DeepClone = require('../../src/functions/deep-clone')
 
 const funcs = mapClassStaticMethods(DeepClone)
 
-describe.each(funcs)('Function', (func) => {
+describe.each(funcs)('Functions', (func) => {
   describe(`🟣 ${func.name} `, () => {
     it('should clone a function', () => {
       function foo(a, b) {
@@ -17,6 +17,24 @@ describe.each(funcs)('Function', (func) => {
       assert.ok(clone(a, b) === foo(a, b))
     })
 
+    it('should clone the function name', () => {
+      function foo(a, b) {
+        return a + b
+      }
+      let clone = func(foo)
+      assert.ok(clone.name === 'foo')
+    })
+
+    it('should deep clone a function', () => {
+      function foo(a, b) {
+        return a + b
+      }
+      let clone = func(foo)
+      assert.deepEqual(clone, foo)
+      foo.bar = '111'
+      assert.notDeepEqual(clone, foo)
+    })
+
     it('should clone a function own properties', () => {
       function foo(a, b) {
         return a + b
@@ -24,6 +42,34 @@ describe.each(funcs)('Function', (func) => {
       foo.bar = 'aa'
       let clone = func(foo)
       assert.ok(clone.bar === foo.bar)
+    })
+
+    it('should clone the prototype', () => {
+      function Person(name, age, gender) {
+        this.name = name
+        this.age = age
+        this.gender = gender
+      }
+      Person.prototype.getName = function () {
+        return this.name
+      }
+
+      let clone = func(Person)
+      assert.ok(clone.prototype === Person.prototype)
+    })
+
+    it('should clone the constructor', () => {
+      function Person(name, age, gender) {
+        this.name = name
+        this.age = age
+        this.gender = gender
+      }
+      Person.prototype.getName = function () {
+        return this.name
+      }
+
+      let clone = func(Person)
+      assert.ok(clone.constructor.name === Person.constructor.name)
     })
   })
 })
